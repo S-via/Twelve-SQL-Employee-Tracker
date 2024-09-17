95bf5ce13a4bcc6cbd2778f451bfc13026e1352c
S-via <168160925+S-via@users.noreply.github.com>
2024-09-17 15:49:51 -0700

    pseduo code for prompt inquirer

--- server.js
+++ server.js
@@ -12,8 +12,23 @@ app.use(express.json());
 // Path to Pool
 // Connect to data base with Pool
 
-// inquirer prompt 
+// inquirer.prompt()
 
-/* {type:'list' name:'', message:'What would you like to do?', choices:['View all departments','View all roles','View all employees','Add a department','Add a role','add an employee,'Update an employee role']},
+/* 
+{type:'list' 
+name:'options', 
+message:'What would you like to do?', 
+choices:
+['View all departments',
+'View all roles',
+'View all employees',
+'Add a department',
+'Add a role',
+'Add an employee,
+'Update an employee role']},
 /* 
 
+// Choice: View all departments  >> Table of >> Department names Department ids
+// Choice : View all roles >> Job title , role id , department role belongs to , salary for that role
+// Choice: View all employees >> Table of >> Employee Data , Employee ids , First names, Last Names, Job Titles, Departments, Salaries and Managers employee report to 
+// Choice : Add a department  >> Prompt >>
