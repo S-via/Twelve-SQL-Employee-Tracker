7ef153a5152d48c7e8704f220be9e03cb74cd9eb
S-via <168160925+S-via@users.noreply.github.com>
2024-09-18 16:05:15 -0700

    add a switch case for each answers that user will be prompted added pseudo code for each functon that will be created

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 // Entry Point 
-const express = requirer('express');
+const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const fs = requirer('fs');
-const inquirer = requirer('inquirer');
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // Middleware 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Path to Pool
@@ -14,24 +14,63 @@ app.use(express.json());
 
 // inquirer.prompt()
 
-/* 
-{type:'list' 
-name:'options', 
-message:'What would you like to do?', 
-choices:
-['View all departments',
-'View all roles',
-'View all employees',
-'Add a department',
-'Add a role',
-'Add an employee,
-'Update an employee role']}
-/* 
+inquirer.prompt([
+
+    {
+        type: 'list',
+        name: 'options',
+        message: 'What would you like to do?',
+        choices:
+            ['View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role',]
+    }])
+    /// Use switch case for the answers////
+    .then(answers => {
+        switch (answers.options) {
+            case 'View all departments':
+                console.log('');
+                /// function //
+                break;
+
+            case 'View all employees':
+                console.log('');
+                /// function //
+                break;
+            case 'Add a department':
+                console.log('');
+                /// function //
+                break;
+            case 'Add a role':
+                console.log('');
+                /// function //
+                break;
+            case 'Add an employee':
+                console.log('');
+                /// function //
+                break;
+            case 'Update an employee role':
+                console.log('');
+                /// function //
+                break;
+        }
+    });
+
+
+// make a function for each answers  //
+/*
+// pass in the table into this function 
+viewDepartments();
+
+
+/*
+
 
 // if Choice is > View all departments  > then show > FORMATTED TABLE OF > with > Department names , Department ids
 // if Choice is > View all roles >> Job title , role id , department role belongs to , salary for that role
 //if Choice is  > View all employees >> Table of >> Employee Data , Employee ids , First names, Last Names, Job Titles, Departments, Salaries and Managers employee report to 
 // if Choice is > Add a department  >> Prompt >>
-
-/// Use switch case for the answers////
-// make a function for each answers  //
